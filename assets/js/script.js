58f75e691d4e4b09286986e71ae01a1f04cd7802
Guilherme Ferreira <devguilhermeferreira@gmail.com>
2023-01-15 09:38:25 -0300

    Ajustando detalhes
    possivelmente acrescentarei novas features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,13 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
-let iconsCheck=[];
-let iconsTrash=[];
 
-// iconsCheck.map((element)=>{
-//    element.addEventListener('click', (evt)=>{
-//       console.log(evt)
-//    });
-// });
-
-function checked(evt) {
-   const tarefa = evt.target.parentElement.parentElement;
+function checked(e) {
+   const tarefa = e.target.parentElement.parentElement;
    console.log(tarefa)
    tarefa.classList.toggle('checked');
 }
-function trashed(evt) {
-   const tarefa = evt.target.parentElement.parentElement;
+function trashed(e) {
+   const tarefa = e.target.parentElement.parentElement;
    const tarefas = tarefa.parentElement;
    tarefas.removeChild(tarefa);
 }
@@ -24,40 +16,40 @@ button.addEventListener('click', ()=>{
    const nomeDaTarefa = input.value;
    const tarefas = document.querySelector('div.tarefas');
 
-   // Criação dos elementos
-   const tarefa = document.createElement('div');
-   const p = document.createElement('p');
-   const divIcons = document.createElement('div');
-   const iconCheck = document.createElement('i');
-   const iconTrash = document.createElement('i');
+   if (nomeDaTarefa === '' || nomeDaTarefa.length===0) {
+      input.classList.add('error');
+   } else {
+      input.classList.remove('error');
+      // Criação dos elementos
+      const tarefa = document.createElement('div');
+      const p = document.createElement('p');
+      const divIcons = document.createElement('div');
+      const iconCheck = document.createElement('i');
+      const iconTrash = document.createElement('i');
 
-   tarefa.classList.add('tarefa');
+      tarefa.classList.add('tarefa');
 
-   p.innerHTML = nomeDaTarefa;
+      p.innerHTML = nomeDaTarefa;
 
-   divIcons.classList.add('icons');
+      divIcons.classList.add('icons');
 
-   iconCheck.classList.add('fa-solid');
-   iconCheck.classList.add('fa-check');
-   iconCheck.addEventListener('click', checked);
-   iconTrash.classList.add('fa-solid');
-   iconTrash.classList.add('fa-trash');
-   iconTrash.addEventListener('click', trashed);
+      iconCheck.classList.add('fa-solid');
+      iconCheck.classList.add('fa-check');
+      iconTrash.classList.add('fa-solid');
+      iconTrash.classList.add('fa-trash');
 
-   divIcons.appendChild(iconCheck);
-   divIcons.appendChild(iconTrash);
-   tarefa.appendChild(p);
-   tarefa.appendChild(divIcons);
-   tarefas.appendChild(tarefa);
+      iconCheck.addEventListener('click', checked);
+      iconTrash.addEventListener('click', trashed);
 
-   // iconsCheck.push(iconCheck);
-   // iconsTrash.push(iconTrash);
+      divIcons.appendChild(iconCheck);
+      divIcons.appendChild(iconTrash);
+      tarefa.appendChild(p);
+      tarefa.appendChild(divIcons);
+      tarefas.appendChild(tarefa);
 
-   // iconsCheck.map((element)=>{
-   //    element.addEventListener('click', checked);
-   // });
-   // iconsTrash.map((element)=>{
-   //    element.addEventListener('click', trashed);
-   // });
+      // Clear and select input
+      input.value='';
+      input.select();
+   }
    
 });
